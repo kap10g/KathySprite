b94b179c08e727b1d0e185e31fc2a156e5979e66
George Rosar <grosar@Georges-iMac.local>
2014-05-29 20:21:34 -0500

    Initial Commit

---
+++ SpriteTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteTest
+//
+//  Created by George Rosar on 5/29/14.
+//  Copyright (c) 2014 George Rosar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SpriteTest
+//
+
+//  Copyright (c) 2014 George Rosar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
